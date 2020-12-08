581389fe9e8d1decd2ebfb3818f7c67cded5a50e
juan-carlos-correa <juan.correa.herrera@gmail.com>
2020-12-08 08:25:14 -0600

    added custom matcher example

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -1,5 +1,24 @@
 import { calculator } from "./index";
 
+expect.extend({
+  toBeNumberTwo(received) {
+    if (received !== 2) {
+      return {
+        pass: false,
+        message: () => `Expected ${received} to be number 2`,
+      };
+    }
+
+    return {
+      pass: true,
+    };
+  },
+});
+
 test("sum", () => {
   expect(calculator.sum(1, 2)).toEqual(3);
+});
+
+test("custom matcher", () => {
+  expect(2).toBeNumberTwo();
 });
